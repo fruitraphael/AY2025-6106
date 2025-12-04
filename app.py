ee8d4c4e33c7bcb2e83e285075c1daff5ebc79d9
fruitraphael <yixuanwang0419@gmail.com>
2025-12-04 08:39:17 +0000

    1

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
-def index():
-    return(render_template("index.html"))
-
-
-@app.route("/main",methods=["GET","POST"])
 def main():
-    q= request.form.get("q")
+    q = request.form.get("q")
     print(q)
     return(render_template("main.html"))
-
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
 if __name__=="__main__":
     app.run()
